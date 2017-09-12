adc1e9f3f0c41f6c7ca5426cf51e0243f60c4e5a
Victor Carvalho <victor.blq@gmail.com>
2017-09-12 00:29:54 -0300

    correção background

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,7 @@ var RtProgressBarComponent = (function () {
 RtProgressBarComponent.decorators = [
     { type: Component, args: [{
                 selector: 'rt-progress-bar',
-                template: "<div class=\"row\" layout-align=\"center center\"> <div class=\"meter\" style=\"background: #{{fgColor}};\" > <span class=\"progressParent\" [style.width.%]=\"percentage\"> <span class=\"progress\" style=\"background: #{{bgColor}};\"></span> </span> </div> </div>",
+                template: "<div class=\"row\" layout-align=\"center center\"> <div class=\"meter\" [style.background.color]=\"fgColor\" > <span class=\"progressParent\" [style.width.%]=\"percentage\"> <span class=\"progress\" [style.background.color]=\"bgColor\"></span> </span> </div> </div>",
                 styles: [".row { -webkit-box-orient: horizontal; -webkit-box-direction: normal; -ms-flex-direction: row; flex-direction: row; display: flex; padding: 0px 40px 10px 40px; } .meter {  width: 100%; display: block; height: 30px; position: relative; overflow: hidden; } .meter span { display: block; height: 100%; } .progress { -webkit-animation: progressBar 1s ease-in-out; -webkit-animation-fill-mode:both;  -moz-animation: progressBar 1s ease-in-out; -moz-animation-fill-mode:both; } .progressParent { width: 0px; -webkit-transition: width 1s; -moz-transition: width 1s;  transition: width 1s; } @-webkit-keyframes progressBar { 0% { width: 0; } 100% { width: 100%; } } @-moz-keyframes progressBar { 0% { width: 0; } 100% { width: 100%; } }"]
             },] },
 ];
