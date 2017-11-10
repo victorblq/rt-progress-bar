25817955f7079e9cd42604709f9115d2dcf68a91
Victor Carvalho <victor.blq@gmail.com>
2017-11-10 15:02:43 -0200

    now removing the folder

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-export * from './rt-progress-bar.component';
-export declare class RtProgressBarModule {
-    static forRoot(): ModuleWithProviders;
-}
